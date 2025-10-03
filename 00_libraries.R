9422a348112eaa03b01a56f5bc241e58245a020e
Ethan Milne <90465566+toodaltat@users.noreply.github.com>
2025-10-03 13:38:13 +1300

    Tidying code base and commenting throughout

--- 00_libraries.R
+++ 00_libraries.R
@@ -21,7 +21,7 @@ model_path <- "C:\\Users\\Ethan\\OneDrive\\Cloud_Desktop\\00_UC\\00_2025_S2\\00_
 udmodel <- udpipe_load_model(file = model_path)
 
 ################################################################################
-# Sentence extraction
+# Function for sentence extraction
 ################################################################################
 
 # Used in file '02_processing.Rmd'
@@ -35,3 +35,95 @@ extract_first_sentence <- function(texts) {
   first_sentences <- trimws(split_matrix[, 1])
   return(first_sentences)
 }
+
+################################################################################
+# Functions for survey handling
+################################################################################
+
+load_raw_data <- function(path) {
+  readr::read_csv(path, col_names = FALSE,
+                  show_col_types = FALSE)
+}
+
+extract_questions <- function(raw) {
+  names_short <- unlist(raw[1, ])
+  names_long  <- unlist(raw[2, ])
+  
+  tibble(
+    question_code = names_short[grep("^Q[0-9]", names_short)],
+    question_text = names_long[grep("^Q[0-9]", names_short)]
+  ) %>%
+    mutate(across(everything(), as.character))
+}
+
+
+reshape_data <- function(raw, questions) {
+  # Removes header rows and assigns column names
+  names_short <- unlist(raw[1, ])
+  df <- raw[-c(1, 2), ]
+  names(df) <- names_short
+  # Reshapes to long  
+  df %>%
+    pivot_longer(
+      cols = starts_with("Q"),
+      names_to = "question_code",
+      values_to = "response"
+    ) %>%
+    left_join(questions, by = "question_code") %>%
+    select(ResponseId, StartDate, EndDate, Age, Origin,
+           question_code, response) %>%
+    rename(
+      response_id = ResponseId,
+      start_date  = StartDate,
+      end_date    = EndDate,
+      age         = Age,
+      origin      = Origin
+    )
+}
+
+clean_data <- function(df_long, group_lookup) {
+  df_long %>%
+    slice(-(1:19)) %>%
+    mutate(
+      question_code = str_remove(question_code, "_1$"),
+      # Applies question grouping
+      question_code = if_else(
+        question_code %in% names(group_lookup),
+        paste0(question_code, "_", group_lookup[question_code]),
+        question_code
+      )
+    ) %>%
+    # Removing practice question and empty responses
+    filter(!is.na(response), response != "", question_code != "Q0") %>%
+    mutate(
+      response = as.numeric(response),
+      across(where(is.character), str_trim),
+      # Standardizing origin
+      origin = case_when(
+        str_to_lower(origin) == "nz" ~ "New Zealand",
+        TRUE ~ origin
+      ),
+      # Categorizing response
+      category = case_when(
+        response <= 3 ~ "low",
+        response <= 6 ~ "mid",
+        response >= 7 ~ "high",
+        TRUE ~ NA_character_
+      ),
+      # Extracting last char as mod_factor
+      mod_factor = str_sub(question_code, -1)
+    )
+}
+
+# Defining questions category
+groupings <- list(
+  TT = c("Q4", "Q9", "Q15", "Q17"),
+  TF = c("Q1", "Q5", "Q8", "Q14"),
+  FT = c("Q2", "Q6", "Q11", "Q12", "Q13"),
+  FF = c("Q3", "Q7", "Q10", "Q16", "Q18")
+)
+
+group_lookup <- map2(groupings, names(groupings),
+                     ~ setNames(rep(.y, length(.x)), .x)) %>%
+  flatten_chr()
+
